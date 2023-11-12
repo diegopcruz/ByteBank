2f69f04da41bf3060ad4859c7351173b09078b13
Diego Cruz <diegopcruz@outlook.com>
2023-11-12 18:07:15 -0300

    first commit

---
+++ 06-ByteBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Channels;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Guilherme";
+            cliente.CPF = "434.564.879-20";
+            cliente.Profissao = "Desenvolvedor";
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+        }                
+    }
+
+}
